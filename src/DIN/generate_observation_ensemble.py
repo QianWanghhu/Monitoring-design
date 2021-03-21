affe4fc5bf709bc277234121ce73a4fd7264aedd
QianW <qian@hhu.edu.cn>
2021-03-21 17:14:51 +0800

    create run_default_obs and run_obs_ensemble

--- src/DIN/generate_observation_ensemble.py
+++ src/DIN/generate_observation_ensemble.py
@@ -10,14 +10,34 @@ import time
 import os
 import spotpy as sp
 
-from funcs.modeling_funcs import change_param_values, obtain_initials, modeling_settings, vs_settings
-from funcs.modeling_funcs import generate_observatio_noised, generate_observation_ensemble
-from funcs.modeling_funcs import generate_parameter_ensemble
+from funcs.modeling_funcs import vs_settings, obtain_initials,\
+    change_param_values, generate_observation_ensemble, generate_parameter_ensemble,\
+        generate_observation_default, modeling_settings
+
+# check if there is a file containing synthetic observations
+def run_default_obs(vs, criteria, start_date, end_date, 
+    obs_name, retrieve_time, datapath):
+    if not os.path.exists(f'{datapath}{obs_name}.csv'):
+        load = generate_observation_default(vs_list[0], criteria, start_date, 
+            end_date, retrieve_time)
+        load.to_csv(f'{datapath}{obs_name}.csv')
+    else:
+        print(f'{datapath}.csv exists.')
+
+# generate the observation ensemble
+def run_obs_ensemble(vs, criteria, start_date, end_date, parameters, 
+    obs_ensemble_name, retrieve_time, datapath):
+    if not os.path.exists(f'{datapath}{obs_ensemble_name}.csv'):
+        load = generate_observation_ensemble(vs_list, criteria, 
+            start_date, end_date, parameters, retrieve_time)
+        load.to_csv(f'{datapath}{obs_ensemble_name}.csv')
+    else:
+        print(f'{obs_ensemble_name}.csv exists.')
 
 first_port=15000
-num_copies = 8
+num_copies = 1
 # define catchment_project and veneer_exe
-project_path = 'src/DIN/pest_source/'
+project_path = 'pest_source/'
 # project_path = 'pest_source/'
 catchment_project= project_path + '/MW_BASE_RC10.rsproj'
 
@@ -25,7 +45,7 @@ catchment_project= project_path + '/MW_BASE_RC10.rsproj'
 # define paths to veneer command and the catchment project
 veneer_path = project_path + 'vcmd45/FlowMatters.Source.VeneerCmd.exe'
 #Now, go ahead and start source
-processes,ports = start(catchment_project,
+processes, ports = start(catchment_project,
                         n_instances=num_copies,
                         ports=first_port,
                         debug=True,
@@ -37,22 +57,29 @@ NODEs, things_to_record, criteria, start_date, end_date = modeling_settings()
 vs_list = vs_settings(ports, things_to_record)
 
 # generate parameter emsenble
-datapath = 'data/'
+datapath = '../../data/'
 nsample = 512
-param_ensemble = 'src/DIN/parameter_ensemble.csv'
+param_ensemble = 'parameter_ensemble_test.csv'
 generate_parameter_ensemble(nsample, param_ensemble, datapath, seed=88)
 
-# check if there is a file containing synthetic observations
-if not os.path.exists(f'{datapath}observation.csv'):
-    generate_observatio_noised(vs_list[0], criteria, start_date, end_date)
-else:
-    print('observation.csv exists.')
+# obtain the initial values of parameter 
+initial_values = obtain_initials(vs_list[0])
 
-# generate the observation ensemble
-if not os.path.exists(f'{datapath}din_daily.csv'):
-    generate_observation_ensemble(vs_list, criteria, start_date, end_date)
-else:
-    print('din_daily.csv exists.')
+# run to generate observation with default parameter values in the model
+obs_name = 'observation_test'
+retrieve_time = [pd.Timestamp('2015-07-01'), pd.Timestamp('2016-06-30')]
+run_default_obs(vs_list[0], criteria, start_date, end_date, 
+    obs_name, retrieve_time, datapath)
+
+# run to generate observation ensemble with parameter ensemble
+obs_ensemble_name = 'din_daily_test'   
+parameters = pd.read_csv('parameter_ensemble_test.csv', index_col='real_name')[0:2] #OED/src/DIN/
+run_obs_ensemble(vs_list, criteria, start_date, end_date, parameters, 
+    obs_ensemble_name, retrieve_time, datapath)
+
+# set parameter to the initial values
+for vs in vs_list:
+    vs = change_param_values(vs, initial_values, fromList=True)
 
 kill_all_now(processes)
 
