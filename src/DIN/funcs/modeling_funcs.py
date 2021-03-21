74601a05f9ea5a243ffa8c91fcbc1b9bc79ad217
QianW <qian@hhu.edu.cn>
2021-03-21 17:11:50 +0800

    Fix the bug in modeling_settings

--- src/DIN/funcs/modeling_funcs.py
+++ src/DIN/funcs/modeling_funcs.py
@@ -11,34 +11,82 @@ import spotpy as sp
 import SALib
 from SALib.sample import latin
 
-def generate_observatio_noised(vs, criteria, start_date, end_date):
-    # install the results into a csv
-    load = pd.DataFrame()
-    print('Run the model to produce synthetic observations')
+def modeling_settings():
+    """
+    Set and return user_defined arguments for model configuration, recording and retrieving results.
+    Returns:
+    NODEs: list, list of elements to record
+    things_to_record, list of dict, to confifure the model
+    criteria: dict, criteria used to filter results
+    start_date, end_date
+    """
+    NODEs = ['gauge_126001A_SandyCkHomebush']
+    things_to_record = [{'NetworkElement':node,'RecordingVariable':'Constituents@N_DIN@Downstream Flow Mass'} for node in NODEs]
+    criteria = {'NetworkElement': NODEs[0],'RecordingVariable':'Constituents@N_DIN@Downstream Flow Mass'}
+    start_date = '01/07/2014'; end_date='01/07/2016'
+    assert isinstance(start_date, str),"start_date has to be time str."
+    assert isinstance(end_date, str),"end_date has to be time str."
+    assert isinstance(things_to_record, list),"things_to_record has to be a list of dict."
+    return NODEs, things_to_record, criteria, start_date, end_date
 
+
+def generate_observation_default(v, criteria, start_date, end_date, retrieve_time):
+    """
+    Run the model to obtain observations without noise.
+    Parameters:
+    ===========
+    vs: veneer object
+    criteria: dict, criteria used to configure the model
+    start_date, end_date: str of dates when the model simulates, e.g., 
+        start_date = '01/07/2014'; end_date='01/07/2016'
+
+    Returns:
+    ===========
+    load: pd.DataFrame, loads to return
+    """
+    # store the results into a .csv file
     time_start = time.time()
-    vs.drop_all_runs()
-    vs.run_model(params={'NoHardCopyResults':True}, start = start_date, end = end_date) 
+    load = pd.DataFrame()
+    assert isinstance(v, veneer.general.Veneer),"vs has to be an veneer object."
     
+    assert isinstance(retrieve_time, list),"retrieve_time has to be a list of timestamp."
+    
+    print('-----------Run the model to produce synthetic observations-----------')
+    v.drop_all_runs()
+    v.run_model(params={'NoHardCopyResults':True}, start = start_date, end = end_date) 
     column_names = ['DIN_obs_load']
-    get_din = vs.retrieve_multiple_time_series(criteria=criteria)
+    # import pdb; pdb.set_trace()
+    get_din = v.retrieve_multiple_time_series(criteria=criteria)
     get_din.columns = column_names
-    din = get_din.loc[pd.Timestamp('2010-07-01'):pd.Timestamp('2016-06-30')]
+    din = get_din.loc[retrieve_time[0]:retrieve_time[1]]
     # store the daily results and the index of sampling
     load = pd.concat([load, din], axis=1)
+    v.drop_all_runs()
     time_end = time.time()
     print(f'{time_end - time_start} seconds')
-    load.to_csv('src/data/observation.csv')
-    vs.drop_all_runs()
-    print('----------Finished-----------')
+    print('----------Finished generate_observation_noised-----------')
+    return load
+# End generate_observation_noised()
 
+def generate_observation_ensemble(vs_list, criteria, start_date, end_date, parameters, retrieve_time):
+    """
+    Run the model to obtain observations without noise.
+    Parameters:
+    ===========
+    vs: veneer object
+    criteria: dict, criteria used to configure the model
+    start_date, end_date: str of dates when the model simulates, e.g., 
+        start_date = '01/07/2014'; end_date='01/07/2016'
 
-def generate_observation_ensemble(vs_list, criteria, start_date, end_date):
+    Returns:
+    ===========
+    load: pd.DataFrame, loads to return
+    """
+    assert isinstance(vs_list, list),"vs has to be a list of veneer objects."
     num_copies = len(vs_list)     
     # install the results into a csv
     load = pd.DataFrame()
     time_start = time.time()
-    parameters = pd.read_csv('src/DIN/parameter_ensemble.csv', index_col='real_name') #OED/src/DIN/
     # parameters = parameters.iloc[0:12, :]
     num_runs = parameters.shape[0]
     group_loops = np.floor_divide(num_runs, num_copies) + 1
@@ -49,6 +97,7 @@ def generate_observation_ensemble(vs_list, criteria, start_date, end_date):
             num_copies_loop = (num_runs - index_group * num_copies)
         else:
             num_copies_loop = num_copies
+
         for j in range(num_copies_loop):
             total_runs += 1
             if (index_group * num_copies + j) >= num_runs: break
@@ -71,22 +120,18 @@ def generate_observation_ensemble(vs_list, criteria, start_date, end_date):
             column_names = ['DIN_' + str(run_index)]
             get_din = vs.retrieve_multiple_time_series(criteria=criteria)
             get_din.columns = column_names
-            din = get_din.loc[pd.Timestamp('2010-07-01'):pd.Timestamp('2016-06-30')]
+            din = get_din.loc[retrieve_time[0]:retrieve_time[1]]
             # store the daily results and the index of sampling
             load = pd.concat([load, din], axis=1)
         
         print(f'Finish {total_runs} runs')
 
-    # set the parameter to initial values
-    for vs in vs_list:
-        initial_values = obtain_initials(vs)
-        vs = change_param_values(vs, initial_values)
-
     # kill_all_now()
     time_end = time.time()
     print(f'{time_end - time_start} seconds')
-
-    load.to_csv('src/data/din_daily.csv')
+    print('----------Finished generate_observation_ensemble-----------')
+    return load
+# End generate_observation_ensemble() 
 
 def generate_parameter_ensemble(nsample, param_ensemble, datapath, seed=None):
     """
@@ -118,21 +163,22 @@ def generate_parameter_ensemble(nsample, param_ensemble, datapath, seed=None):
     else:
         print(f'The file of parameter ensemble exists under the folder')
 
-def change_param_values(v, pvalue_dict):
-    v.model.catchment.generation.set_param_values('DeliveryRatioSurface',pvalue_dict['DRF'],  fus=['Sugarcane'])
-    v.model.catchment.generation.set_param_values('DeliveryRatioSeepage',pvalue_dict['DRP'],  fus=['Sugarcane'])
-    v.model.catchment.generation.set_param_values('DWC', pvalue_dict['DWC'], fus=['Sugarcane'])
-    v.model.catchment.generation.set_param_values('Load_Conversion_Factor', pvalue_dict['LCF'], fus=['Sugarcane'])
-    v.model.catchment.generation.set_param_values('dissConst_DWC', pvalue_dict['gfDWC'], fus=['Grazing Forested'])
-    v.model.catchment.generation.set_param_values('dissConst_EMC', pvalue_dict['gfEMC'], fus=['Grazing Forested'])
-    v.model.catchment.generation.set_param_values('dissConst_DWC', pvalue_dict['goDWC'], fus=['Grazing Open'])
-    v.model.catchment.generation.set_param_values('dissConst_EMC', pvalue_dict['goEMC'], fus=['Grazing Open'])
-    v.model.catchment.generation.set_param_values('dissConst_DWC', pvalue_dict['cDWC'], fus=['Conservation'])
-    v.model.catchment.generation.set_param_values('dissConst_EMC', pvalue_dict['cEMC'], fus=['Conservation'])
-    v.model.catchment.generation.set_param_values('dissConst_DWC', pvalue_dict['fDWC'], fus=['Forestry'])
-    v.model.catchment.generation.set_param_values('dissConst_EMC', pvalue_dict['fEMC'], fus=['Forestry'])
-    v.model.catchment.generation.set_param_values('dissConst_DWC', pvalue_dict['oDWC'], fus=['Horticulture', 'Urban', 'Water', 'Other', 'Irrigated Cropping'])
-    v.model.catchment.generation.set_param_values('dissConst_EMC', pvalue_dict['oEMC'], fus=['Horticulture', 'Urban', 'Water', 'Other', 'Irrigated Cropping'])
+def change_param_values(v, pvalue_dict, fromList=False):
+    assert isinstance(v, veneer.general.Veneer),"vs has to be an veneer object."
+    v.model.catchment.generation.set_param_values('DeliveryRatioSurface',pvalue_dict['DRF'],  fus=['Sugarcane'], fromList=fromList)
+    v.model.catchment.generation.set_param_values('DeliveryRatioSeepage',pvalue_dict['DRP'],  fus=['Sugarcane'], fromList=fromList)
+    v.model.catchment.generation.set_param_values('DWC', pvalue_dict['DWC'], fus=['Sugarcane'], fromList=fromList)
+    v.model.catchment.generation.set_param_values('Load_Conversion_Factor', pvalue_dict['LCF'], fus=['Sugarcane'], fromList=fromList)
+    v.model.catchment.generation.set_param_values('dissConst_DWC', pvalue_dict['gfDWC'], fus=['Grazing Forested'], fromList=fromList)
+    v.model.catchment.generation.set_param_values('dissConst_EMC', pvalue_dict['gfEMC'], fus=['Grazing Forested'], fromList=fromList)
+    v.model.catchment.generation.set_param_values('dissConst_DWC', pvalue_dict['goDWC'], fus=['Grazing Open'], fromList=fromList)
+    v.model.catchment.generation.set_param_values('dissConst_EMC', pvalue_dict['goEMC'], fus=['Grazing Open'], fromList=fromList)
+    v.model.catchment.generation.set_param_values('dissConst_DWC', pvalue_dict['cDWC'], fus=['Conservation'], fromList=fromList)
+    v.model.catchment.generation.set_param_values('dissConst_EMC', pvalue_dict['cEMC'], fus=['Conservation'], fromList=fromList)
+    v.model.catchment.generation.set_param_values('dissConst_DWC', pvalue_dict['fDWC'], fus=['Forestry'], fromList=fromList)
+    v.model.catchment.generation.set_param_values('dissConst_EMC', pvalue_dict['fEMC'], fus=['Forestry'], fromList=fromList)
+    v.model.catchment.generation.set_param_values('dissConst_DWC', pvalue_dict['oDWC'], fus=['Horticulture', 'Urban', 'Water', 'Other', 'Irrigated Cropping'], fromList=fromList)
+    v.model.catchment.generation.set_param_values('dissConst_EMC', pvalue_dict['oEMC'], fus=['Horticulture', 'Urban', 'Water', 'Other', 'Irrigated Cropping'], fromList=fromList)
     return v
 
 # obtain initial values
@@ -146,6 +192,8 @@ def obtain_initials(v):
     ========
     initial_values: dict, initla values of parameters
     """
+    assert isinstance(v, veneer.general.Veneer),"vs has to be an veneer object."
+
     initial_values = {}
     initial_values['DRF'] = v.model.catchment.generation.get_param_values('DeliveryRatioSurface', fus=['Sugarcane'])
     initial_values['DRP'] = v.model.catchment.generation.get_param_values('DeliveryRatioSeepage',  fus=['Sugarcane'])
@@ -164,14 +212,6 @@ def obtain_initials(v):
     
     return initial_values
 
-def modeling_settings():
-    NODEs = ['126001A']
-    things_to_record = [{'NetworkElement':node,'RecordingVariable':'Constituents@N_DIN@Downstream Flow Mass'} for node in NODEs]
-    criteria = {'NetworkElement':'126001A','RecordingVariable':'Constituents@N_DIN@Downstream Flow Mass'}
-    start_date = '01/07/2008'; end_date='01/07/2016'
-    return NODEs, things_to_record, criteria, start_date, end_date
-
-
 def vs_settings(ports, things_to_record):
     """
     Set up the vs objects.
@@ -185,7 +225,10 @@ def vs_settings(ports, things_to_record):
     vs_list: list, list of vs ojeects
 
     """
+    
     vs_list = []
+    assert isinstance(ports, list),"vs has to be a list of int."
+
     for veneer_port in ports:
         # veneer_port = first_port 
         vs_list.append(veneer.Veneer(port=veneer_port))
