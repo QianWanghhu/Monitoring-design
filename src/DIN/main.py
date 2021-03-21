e15a911189d6856ccbd3f810537e4bbe21df4f87
QianW <qian@hhu.edu.cn>
2021-03-21 17:15:09 +0800

    Update data path

--- src/DIN/main.py
+++ src/DIN/main.py
@@ -9,18 +9,19 @@ import pandas as pd
 from funcs.prep_template import pre_ies_obs_template, format_obs_ensemble
 
 # format ies inputs
-datapath = 'data/'
-fname = f'{datapath}observation.csv'
-if not os.path.exists(fname):    
+datapath = '../../data/'
+fname = f'{datapath}observation_test.csv'
+fname_obs_pst = 'obs_pst_test.csv'
+if not os.path.exists(fname_obs_pst):    
     load = pd.read_csv(fname, index_col='Unnamed: 0')
-    obs_pst_name = f'{datapath}obs_pst.csv'
-    pre_ies_obs_template(load).to_csv(obs_pst_name, sep=' ')
+    obs_pst_name = f'{datapath}{fname_obs_pst}'
+    pre_ies_obs_template(load).to_csv(fname_obs_pst, sep=' ')
 
-src_path = 'src/DIN/'
-if not os.path.exists(f'{src_path}observation_ensemble.csv'):
-    obs_ensemble = pd.read_csv(f'{datapath}din_daily.csv', index_col='Unnamed: 0')
+datapath = '../../data/'
+if not os.path.exists(f'observation_ensemble_test.csv'):
+    obs_ensemble = pd.read_csv(f'{datapath}din_daily_test.csv', index_col='Unnamed: 0')
     obs_ensemble = format_obs_ensemble(obs_ensemble)
-    obs_ensemble.to_csv(f'{src_path}observation_ensemble.csv')
+    obs_ensemble.to_csv(f'observation_ensemble_test.csv')
 
 ## import and run run_daily_observed
 print('All data has been prepared for ies and is going to run run_daily_observed.py')
