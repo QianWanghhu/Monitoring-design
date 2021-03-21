ec0e04d6fd33b2995c7427d38fc0d8e5cc8e82f5
QianW <qian@hhu.edu.cn>
2021-03-21 10:22:07 +0800

    Replace codes for changing parameter values with a method.

--- src/DIN/run_source.py
+++ src/DIN/run_source.py
@@ -1,3 +1,6 @@
+"""
+Script used to run_source and return the output file.
+"""
 import veneer
 from veneer.pest_runtime import *
 from veneer.manage import start,kill_all_now
@@ -6,6 +9,7 @@ import time
 import os
 import spotpy as sp
 
+from funcs.modeling_funcs import change_param_values, modeling_settings
 
 output_file = 'output.txt'
 veneer_port = find_port() 
@@ -19,37 +23,18 @@ for i,j in parameters.iterrows():
     scaled_value = (j.upper - j.lower) * j.value/100 + j.lower 
     parameter_dict[j.parameter] = scaled_value
     
-    
-NODEs = ['126001A']
-things_to_record = [{'NetworkElement':node,'RecordingVariable':'Constituents@N_DIN@Downstream Flow Mass'} for node in NODEs]
+# define the modeling period and the recording variables
+NODEs, things_to_record, criteria, start_date, end_date = modeling_settings()
 vs.configure_recording(disable=[{}], enable=things_to_record)
 
-vs.model.catchment.generation.set_param_values('DeliveryRatioSurface', parameter_dict['DRF'],  fus=['Sugarcane'])
-vs.model.catchment.generation.set_param_values('DeliveryRatioSeepage', parameter_dict['DRP'],  fus=['Sugarcane'])
-vs.model.catchment.generation.set_param_values('DWC', parameter_dict['DWC'], fus=['Sugarcane'])
-vs.model.catchment.generation.set_param_values('Load_Conversion_Factor', parameter_dict['LCF'], fus=['Sugarcane'])
-vs.model.catchment.generation.set_param_values('dissConst_DWC', parameter_dict['gfDWC'], fus=['Grazing Forested'])
-vs.model.catchment.generation.set_param_values('dissConst_EMC', parameter_dict['gfEMC'], fus=['Grazing Forested'])
-vs.model.catchment.generation.set_param_values('dissConst_DWC', parameter_dict['goDWC'], fus=['Grazing Open'])
-vs.model.catchment.generation.set_param_values('dissConst_EMC', parameter_dict['goEMC'], fus=['Grazing Open'])
-vs.model.catchment.generation.set_param_values('dissConst_DWC', parameter_dict['cDWC'], fus=['Conservation'])
-vs.model.catchment.generation.set_param_values('dissConst_EMC', parameter_dict['cEMC'], fus=['Conservation'])
-vs.model.catchment.generation.set_param_values('dissConst_DWC', parameter_dict['fDWC'], fus=['Forestry'])
-vs.model.catchment.generation.set_param_values('dissConst_EMC', parameter_dict['fEMC'], fus=['Forestry'])
-vs.model.catchment.generation.set_param_values('dissConst_DWC', parameter_dict['oDWC'], fus=['Horticulture', 'Urban', 'Water', 'Other', 'Irrigated Cropping'])
-vs.model.catchment.generation.set_param_values('dissConst_EMC', parameter_dict['oEMC'], fus=['Horticulture', 'Urban', 'Water', 'Other', 'Irrigated Cropping'])
-
-start_date = '01/07/2008'; end_date='01/07/2016'
+vs = change_param_values(vs, parameter_dict)
 vs.drop_all_runs()
 vs.run_model(params={'NoHardCopyResults':True}, start = start_date, end = end_date) 
 
-
+# define the criteria for retrieve multiple_time_series
 column_names = ['Load']
-criteria = {'NetworkElement':'126001A','RecordingVariable':'Constituents@N_DIN@Downstream Flow Mass'}
 get_din = vs.retrieve_multiple_time_series(criteria=criteria)
-
 get_din.columns = column_names
-
 din = get_din.loc[pd.Timestamp('2010-07-01'):pd.Timestamp('2016-06-30')]
 
 with open(output_file, 'w') as f:
