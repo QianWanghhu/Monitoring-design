f6bd4b705e270f22cd91f06e996c5d768569178d
QianW <qian@hhu.edu.cn>
2021-03-26 16:06:29 +0800

    Add the calculation of annual sum.

--- src/DIN/run_source.py
+++ src/DIN/run_source.py
@@ -2,6 +2,7 @@
 Script used to run_source and return the output file.
 """
 import veneer
+import numpy as np
 from veneer.pest_runtime import *
 from veneer.manage import start,kill_all_now
 import pandas as pd
@@ -15,30 +16,35 @@ output_file = 'output.txt'
 veneer_port = find_port() 
 vs = veneer.Veneer(port=veneer_port)
 
-parameters = pd.read_csv('parameters.csv')
 print('Read Parameters')
-# import pdb; pdb.set_trace()
-# Define objective functions
-# Use average annual loads
-def average_annual(data, year_length):
-    """
-    The average annual.
-    """
-    data_ave = data.sum()/year_length
-    return data_ave
-# End average_annual()
+parameters = pd.read_csv('parameters.csv')
 
+# Define objective functions
 # Use annual or monthly loads
-def timeseries_sum(df, temp_scale = 'Y'):
+def timeseries_sum(df, temp_scale = 'annual'):
     """
     Obtain the sum of timeseries of different temporal scale.
     temp_scale: str, default is 'Y', monthly using 'M'
     """
-    sum_126001A = df.resample(temp_scale).sum()
+    assert temp_scale in ['monthly', 'annual'], 'The temporal scale given is not supported.'
+    if temp_scale == 'monthly':
+        sum_126001A = df.resample('M').sum()
+    else:
+        month_126001A = df.resample('M').sum()
+        sum_126001A = pd.DataFrame(index = np.arange(df.index[0].year, df.index[-1].year), 
+            columns=df.columns)
+        for i in range(sum_126001A.shape[0]):
+            sum_126001A.iloc[i, :] = month_126001A.iloc[i*12: (i+1)*12, :].sum()
+
     return sum_126001A
+# End timeseries_sum()
 
 # import observation if the output.txt requires the use of obs.
 # Here the observation can be monitoring data or synthetic data.
+date_range = pd.to_datetime(['2009/07/01', '2016/06/30'])
+observed_din = pd.read_csv('low_interp_flow.csv', index_col='Date')
+observed_din.index = pd.to_datetime(observed_din.index)
+observed_din = observed_din.loc[date_range[0]:date_range[1], :].filter(items=[observed_din.columns[0]]).apply(lambda x: 1000 * x)
 
 parameter_dict = {}
 for i,j in parameters.iterrows():
@@ -54,19 +60,22 @@ vs.drop_all_runs()
 vs.run_model(params={'NoHardCopyResults':True}, start = start_date, end = end_date) 
 
 # define the criteria for retrieve multiple_time_series
-column_names = ['Load']
+column_names = ['din']
 get_din = vs.retrieve_multiple_time_series(criteria=criteria)
 get_din.columns = column_names
-din = get_din.loc[pd.Timestamp('2010-07-01'):pd.Timestamp('2016-06-30')]
+din = get_din.loc[date_range[0]:date_range[1], :]
 
 # obtain the sum at a given temporal scale
-df = timeseries_sum(din, temp_scale = 'Y')
+din_pbias = sp.objectivefunctions.pbias(observed_din[observed_din.columns[0]], din[column_names[0]])
+din_126001A = timeseries_sum(din, temp_scale = 'annual')
 
 with open(output_file, 'w') as f:
+    f.write('---- DIN PBIAS ---- \n')
+    f.write(str(din_pbias) + '\n')
     f.write('---- CONSTITUENT LOADS ----  \n')
 
     f.write('---- DIN LOADS ----  \n')
-    for i, j in df.iterrows():
+    for i, j in din_126001A.iterrows():
         f.write(str(j[0]) + '\n')     
 
 
