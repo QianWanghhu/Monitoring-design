a516e58a8c2a9e4b62da42568ff59a0b11a04789
QianW <qian@hhu.edu.cn>
2021-03-26 16:03:54 +0800

    Adding the functions for preparing inputs for pest-ies

--- src/DIN/main.py
+++ src/DIN/main.py
@@ -6,28 +6,86 @@ adding noise and running IES.
 import os
 import numpy as np
 import pandas as pd
+import spotpy as sp
 from funcs.prep_template import pre_ies_obs_template, format_obs_ensemble, add_noise_obs, generate_obsnme
 
 # format ies inputs
 datapath = '../../data/'
+obgnme = 'DIN'
+
 fname = f'{datapath}observation.csv'
 fname_obs_pst = 'obs_pst_noise10pct.csv'
-obgnme = 'DIN'
-if not os.path.exists(fname_obs_pst):
-    # To CONTINUE: Monthly/annual monitoring as well as introducing noise
-    load = pd.read_csv(fname, index_col='Unnamed: 0')
-    obs_pst_name = f'{datapath}{fname_obs_pst}'
-    load = add_noise_obs(load, noise_level=0.1)
-    obs_index = list(load.index)
+
+
+# if not os.path.exists(fname_obs_pst):
+#     print('Preparing the observation file for ies')
+#     load = pd.read_csv(fname, index_col='Unnamed: 0')
+#     obs_pst_name = f'{datapath}{fname_obs_pst}'
+#     load = add_noise_obs(load, noise_level=0.1)
+# # End if-else
+def observation_ensemble(datapath, ies_obs_name):
+    """Prepare the observation ensemble file for ies"""
+    if not os.path.exists(ies_obs_name):
+        obs_ensemble = pd.read_csv(f'{datapath}din_daily.csv', index_col='Unnamed: 0')
+        obs_ensemble.index = pd.to_datetime(obs_ensemble.index)
+        obs_ensemble_month = obs_ensemble.resample('M').sum()
+        obs_ensemble_annual = pd.DataFrame(index=np.arange(obs_ensemble.index[0].year, obs_ensemble.index[-1].year),
+            columns = obs_ensemble.columns)
+        for i in range(obs_ensemble_annual.shape[0]):
+            obs_ensemble_annual.iloc[i] = obs_ensemble_month.iloc[i*12:(i+1)*12, :].sum(axis=0)
+     
+        obs_ensemble_ies = format_obs_ensemble(obs_ensemble_annual, obgnme)
+        obs_ensemble_ies.to_csv(ies_obs_name)
+    # End if-else
+    # End observation_ensemble()
+
+def process_monitor(datapath):
+    """
+    convert the timeseries (monitoring and modeling) to a required temporal scale.
+    read the monitoring data 
+    (Note that the daily loads is of unit t and need to be converted to kg).
+    return the observation section in the control file for PEST.
+    """
+    fname = datapath + 'low_interp_flow.csv'
+    date_range = pd.to_datetime(['2010/07/01', '2016/06/30'])
+    df = pd.read_csv(fname, index_col='Date')
+    df.index = pd.to_datetime(df.index)
+    df = df.loc[date_range[0]:date_range[1], :].filter(items=[df.columns[0]]).apply(lambda x: 1000 * x)
+    # resample to obtain monthly loads
+    df_month = df.resample('M').sum()
+    df_annual = pd.DataFrame(index = ['pbias', *np.arange(date_range[0].year, date_range[1].year)], columns=df.columns)
+    df_annual.iloc[0, :] = 0.0
+    for i in range(df_annual.shape[0]-1):
+        df_annual.iloc[i+1, :] = df_month.iloc[i*12: (i+1)*12, :].sum()
+    # End for
+
+    obs_index = list(df_annual.index)
     obsnme = generate_obsnme(obs_index, obgnme)
-    obsval = load.noised.values
-    pre_ies_obs_template(obsval, obsnme, obgnme).to_csv(fname_obs_pst, sep=' ')
+    obsval = df_annual.values
+    fname_obs_pst = 'monitor_annual.csv'
 
-datapath = '../../data/'
-if not os.path.exists(f'observation_ensemble.csv'):
-    obs_ensemble = pd.read_csv(f'{datapath}din_daily.csv', index_col='Unnamed: 0')
-    obs_ensemble = format_obs_ensemble(obs_ensemble, obgnme)
-    obs_ensemble.to_csv(f'observation_ensemble.csv')
+    weight = np.array([1 / (df_annual.shape[0] - 1)]).repeat(df_annual.shape[0])
+    weight[0] = 1.5
+    pre_ies_obs_template(obsval, obsnme, obgnme, weight).to_csv(fname_obs_pst, sep=' ')
+    return obsnme
+# End process_monitor()
 
-# convert the timeseries (monitoring and modeling) to a required temporal scale
+def write_instruction(fname, obsnme):
+    with open(fname, 'w') as f:
+        f.write('pif # \n')
+        f.write(f'l2    [{obsnme[0]}]1:25 \n')
+        f.write(f'l3    [{obsnme[1]}]1:25 \n')
+        for i in obsnme[2:]:
+            f.write(f'l1    [{i}]1:25 \n')
+            
+        f.close()
 
+# write the observation section
+obsnme = process_monitor(datapath)
+
+# write the corresponding out instruction file for PEST
+write_instruction('output.ins', obsnme)
+
+# write observation ensemble for ies
+ies_obs_name = 'observation_ensemble_annual.csv'
+observation_ensemble(datapath, ies_obs_name)
