b96fddc0f6b4c223b39c45efb90c0444e01d9f74
Qian Wang <qian@hhu.edu.cn>
2021-03-28 14:51:21 +0800

    Add the calculation of objective functions to observation_ensemble.csv

--- src/DIN/main.py
+++ src/DIN/main.py
@@ -26,7 +26,7 @@ fname_obs_pst = 'obs_pst_noise10pct.csv'
 def observation_ensemble(datapath, ies_obs_name):
     """Prepare the observation ensemble file for ies"""
     if not os.path.exists(ies_obs_name):
-        obs_ensemble = pd.read_csv(f'{datapath}din_daily.csv', index_col='Unnamed: 0')
+        obs_ensemble = pd.read_csv(f'{datapath}din_daily_0918.csv', index_col='Unnamed: 0')
         obs_ensemble.index = pd.to_datetime(obs_ensemble.index)
         obs_ensemble_month = obs_ensemble.resample('M').sum()
         obs_ensemble_annual = pd.DataFrame(index=np.arange(obs_ensemble.index[0].year, obs_ensemble.index[-1].year),
@@ -35,7 +35,10 @@ def observation_ensemble(datapath, ies_obs_name):
             obs_ensemble_annual.iloc[i] = obs_ensemble_month.iloc[i*12:(i+1)*12, :].sum(axis=0)
      
         obs_ensemble_ies = format_obs_ensemble(obs_ensemble_annual, obgnme)
-        obs_ensemble_ies.to_csv(ies_obs_name)
+
+        return obs_ensemble_ies, obs_ensemble
+    else:
+        print('The file exists.')
     # End if-else
     # End observation_ensemble()
 
@@ -47,7 +50,7 @@ def process_monitor(datapath):
     return the observation section in the control file for PEST.
     """
     fname = datapath + 'low_interp_flow.csv'
-    date_range = pd.to_datetime(['2010/07/01', '2016/06/30'])
+    date_range = pd.to_datetime(['2009/07/01', '2018/06/30'])
     df = pd.read_csv(fname, index_col='Date')
     df.index = pd.to_datetime(df.index)
     df = df.loc[date_range[0]:date_range[1], :].filter(items=[df.columns[0]]).apply(lambda x: 1000 * x)
@@ -67,7 +70,7 @@ def process_monitor(datapath):
     weight = np.array([1 / (df_annual.shape[0] - 1)]).repeat(df_annual.shape[0])
     weight[0] = 1.5
     pre_ies_obs_template(obsval, obsnme, obgnme, weight).to_csv(fname_obs_pst, sep=' ')
-    return obsnme
+    return obsnme, df
 # End process_monitor()
 
 def write_instruction(fname, obsnme):
@@ -80,12 +83,27 @@ def write_instruction(fname, obsnme):
             
         f.close()
 
+def calculate_obs_objetive(monitor, obs_ensemble_ies, obs_ensemble):
+    """
+    Calculate objectives with monitoring data and observation_ensemble.
+    """
+    objective_values = np.array([])
+    # import pdb; pdb.set_trace()
+    monitor_comp = monitor[monitor.columns[0]]
+    for col in obs_ensemble.columns:
+        objective_values = np.append(objective_values, sp.objectivefunctions.pbias(obs_ensemble[col], monitor_comp))
+    
+    obs_ensemble_ies.insert(0, 'DIN_pbias', objective_values)
+    return obs_ensemble_ies
 # write the observation section
-obsnme = process_monitor(datapath)
+obsnme, df = process_monitor(datapath)
 
 # write the corresponding out instruction file for PEST
-write_instruction('output.ins', obsnme)
+write_instruction('output_0918.ins', obsnme)
 
 # write observation ensemble for ies
-ies_obs_name = 'observation_ensemble_annual.csv'
-observation_ensemble(datapath, ies_obs_name)
+ies_obs_name = 'observation_ensemble_0918.csv'
+obs_ensemble_ies, obs_ensemble = observation_ensemble(datapath, ies_obs_name)
+
+obs_ensemble_ies = calculate_obs_objetive(df, obs_ensemble_ies, obs_ensemble)
+obs_ensemble_ies.to_csv(ies_obs_name)
